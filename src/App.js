60a20e0d85f473b3b550dd62203f6135f724da44
niko bazylev <bazylevnik0@gmail.com>
2023-06-16 20:08:04 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  let A, B, M;
+  function init() {
+    A = [];
+    B = [];
+    M = [];
+    for (let y = 0; y < 20; y++) {
+      M[y] = [];
+      for (let x = 0; x < 30; x++) {
+        M[y][x] = {};
+      }
+    }
+  }
+  init();
+  const [Avalue, setAvalue] = useState();
+  const [Bvalue, setBvalue] = useState();
+  const [Acolor, setAcolor] = useState();
+  const [Bcolor, setBcolor] = useState();
+  function changeValue(e) {
+    switch (e.target.id) {
+      case "Avalue":
+        setAvalue(e.target.value);
+        break;
+      case "Bvalue":
+        setBvalue(e.target.value);
+        break;
+      case "Acolor":
+        setAcolor(e.target.value);
+        break;
+      case "Bcolor":
+        setBcolor(e.target.value);
+        break;
+    }
+  }
+  function set() {
+    //set A
+    for (let y = 0; y < 20; y++) {
+      A[y] = [];
+      for (let x = 0; x < 20; x++) {
+        A[y][x] = { v: +Avalue };
+      }
+    }
+    //set B
+    for (let y = 0; y < 20; y++) {
+      B[y] = [];
+      for (let x = 0; x < 20; x++) {
+        B[y][x] = { v: +Bvalue };
+      }
+    }
+    //set M to start position
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        M[y][x].a = A[y][x];
+      }
+      for (let x = 10; x < 30; x++) {
+        M[y][x].b = B[y][x - 10];
+      }
+    }
+    draw();
+  }
   function draw() {
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, 10, 10);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 300, 200);
+
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 30; x++) {
+        if (M[y][x].a && M[y][x].a.v == 1) {
+          ctx.fillStyle = "#" + Acolor;
+          ctx.strokeStyle = "black";
+          ctx.fillRect(0 + x * 10, 0 + y * 10, 10, 10);
+          ctx.strokeRect(0 + x * 10, 0 + y * 10, 10, 10);
+          ctx.stroke();
+        }
+        if (M[y][x].b && M[y][x].b.v == 1) {
+          ctx.fillStyle = "#" + Bcolor;
+          ctx.strokeStyle = "black";
+          ctx.fillRect(0 + x * 10, 0 + y * 10, 10, 10);
+          ctx.strokeRect(0 + x * 10, 0 + y * 10, 10, 10);
+          ctx.stroke();
+        }
+      }
+    }
   }
   return (
     <div className="App">
@@ -14,15 +93,42 @@ export default function App() {
       <div className="Box">
         <div className="Input">
           <label>A: </label>
-          <input className="InputValue" type="text" maxlength="1"></input>
-          <input className="InputColor" type="text" maxlength="6"></input>
+          <input
+            id="Avalue"
+            value={Avalue}
+            onChange={changeValue}
+            className="InputValue"
+            type="text"
+            maxLength="1"
+          ></input>
+          <input
+            id="Acolor"
+            value={Acolor}
+            onChange={changeValue}
+            className="InputColor"
+            type="text"
+            maxLength="6"
+          ></input>
           <label>B: </label>
-          <input className="InputValue" type="text" maxlength="1"></input>
-          <input className="InputColor" type="text" maxlength="6"></input>
+          <input
+            id="Bvalue"
+            value={Bvalue}
+            onChange={changeValue}
+            className="InputValue"
+            type="text"
+            maxLength="1"
+          ></input>
+          <input
+            id="Bcolor"
+            value={Bcolor}
+            onChange={changeValue}
+            className="InputColor"
+            type="text"
+            maxLength="6"
+          ></input>
           <br />
-          <button>set</button>
+          <button onClick={set}>set</button>
         </div>
-        <br />
         <div className="Draw">
           <canvas className="Canvas" width="300" height="200"></canvas>
         </div>
